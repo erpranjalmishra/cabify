b42aac4dce4aa92e9a0602f84ff2228a363229ad
Pranjal Mishra <x@pranjalmishra.dev>
2025-04-26 05:20:33 +0530

    Update settings.py

--- CabBooking/CabBooking/settings.py
+++ CabBooking/CabBooking/settings.py
@@ -125,3 +125,5 @@ EMAIL_HOST_PASSWORD = 'kcaq rcdm fdlp tqyx'  # Replace with your Gmail password 
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
